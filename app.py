4e904b717cea3a06d34c828d22879f3d147d8adb
Marialuisabcs <maria.lbcs@icloud.com>
2022-05-25 04:20:57 -0400

    feat: encapsulates main methods

---
+++ app.py
@@ -0,0 +1,28 @@
+from scrapper import *
+import os
+from pathlib import Path
+
+from scrapper.main import Scrapper
+
+
+def main():
+    root_url = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
+    files_to_download = ['Anexo I', 'Anexo II', 'Anexo III', 'Anexo IV']
+
+    root_path = Path.cwd()
+
+    scpr = Scrapper(root_path, root_url, files_to_download)
+
+    links = scpr.get_urls_from_page()
+    pdfs = scpr.get_only_pdf(links)
+
+    for file in files_to_download:
+        pdf = scpr.get_pdf_by_name(pdfs, file)
+        print(f'Downloading file {file}')
+        scpr.download_file(pdf)
+
+    scpr.zip_file('anexos')
+
+
+if __name__ == '__main__':
+    main()
