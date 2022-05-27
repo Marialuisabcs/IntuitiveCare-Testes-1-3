82636c3a1dcb13e2a2eaaacfc296a9162494613e
Marialuisabcs <maria.lbcs@icloud.com>
2022-05-27 02:49:16 -0400

    feat: creates global function to zip files

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,1 +1,15 @@
-# Init if needed
+import shutil
+from pathlib import Path
+
+import colorama
+
+colorama.init()
+GREEN = colorama.Fore.GREEN
+YELLOW = colorama.Fore.YELLOW
+RED = colorama.Fore.RED
+CYAN = colorama.Fore.CYAN
+
+
+def zip_file(output_file_name: str, path: str):
+    zip_file_name = shutil.make_archive(base_name=output_file_name, base_dir=path, format='zip')
+    print(f'{GREEN}[!]{zip_file_name} zipped')
